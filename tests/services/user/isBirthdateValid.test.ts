e8146edf0a6667817268edfde8b8d5e465eb7374
Esdrasmelo <emelomotta@gmail.com>
2023-01-31 11:31:42 -0300

    test: add specific test file for isBirthdateValid method

---
+++ tests/services/user/isBirthdateValid.test.ts
@@ -0,0 +1,35 @@
+import { UserRepositoryImplementation } from "../../../src/repository";
+import { UserService } from "../../../src/services";
+
+const makeSut = () => {
+  const userRepositoryStub = new UserRepositoryImplementation();
+  const sut = new UserService(userRepositoryStub);
+
+  return {
+    sut,
+  };
+};
+
+describe("Is Birthdate Valid method", () => {
+  it("Should return true if birthdate is valid", async () => {
+    const { sut } = makeSut();
+
+    const fakeValidBirthdate = new Date();
+    fakeValidBirthdate.setDate(fakeValidBirthdate.getDate() - 10);
+
+    const sutReturn = sut.IsBirthdateValid(fakeValidBirthdate);
+
+    expect(sutReturn).toBeTruthy();
+  });
+
+  it("Should return false if birthdate is not valid", async () => {
+    const { sut } = makeSut();
+
+    const fakeInvalidBirthdate = new Date();
+    fakeInvalidBirthdate.setDate(fakeInvalidBirthdate.getDate() + 10);
+
+    const sutReturn = sut.IsBirthdateValid(fakeInvalidBirthdate);
+
+    expect(sutReturn).toBeFalsy();
+  });
+});
