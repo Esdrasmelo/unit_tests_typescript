e56c6b3049da45f1cc1a6dc4ca15dcf82f3f4c48
Esdrasmelo <emelomotta@gmail.com>
2023-01-31 11:32:49 -0300

    feat: add new interface type that searchs for user by id

--- src/types/repository/userRepositoryPort.type.ts
+++ src/types/repository/userRepositoryPort.type.ts
@@ -3,7 +3,8 @@ import { IUser, IUserAdd, IUserUpdate } from "../user/user.type";
 export interface IUserRepositoryPort {
   findAllUsers: () => Promise<IUser[]>;
   findUserByEmail: (email: string) => Promise<IUser | undefined>;
-  createUser: (input_data: IUserAdd) => Promise<IUser>;
-  updateUser: (iid: string, input_data: IUserUpdate) => Promise<IUser>;
+  findUserById: (userId: string) => Promise<IUser | undefined>;
+  createUser: (inputData: IUserAdd) => Promise<IUser>;
+  updateUser: (id: string, inputData: IUserUpdate) => Promise<IUser>;
   deleteUser: (id: string) => Promise<IUser>;
 }
