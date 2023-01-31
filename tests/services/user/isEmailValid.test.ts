34251abe93c99e017ae0f7e633212827acf24fa9
Esdrasmelo <emelomotta@gmail.com>
2023-01-31 11:31:48 -0300

    test: add specific test file for isEmailValid method

---
+++ tests/services/user/isEmailValid.test.ts
@@ -0,0 +1,33 @@
+import { UserRepositoryImplementation } from "../../../src/repository";
+import { UserService } from "../../../src/services";
+
+const makeSut = () => {
+  const userRepositoryStub = new UserRepositoryImplementation();
+  const sut = new UserService(userRepositoryStub);
+
+  return {
+    sut
+  };
+};
+
+describe("Is Email Valid method", () => {
+  it("Should return false if email is not valid", async () => {
+    const { sut } = makeSut();
+
+    const fakeInvalidEmail = "invalid_email";
+
+    const sutReturn = sut.IsEmailValid(fakeInvalidEmail);
+
+    expect(sutReturn).toBeFalsy();
+  });
+
+  it("Should return true if email is valid", async () => {
+    const { sut } = makeSut();
+
+    const fakeValidEmail = 'valid_email@email.com';
+
+    const sutReturn = sut.IsEmailValid(fakeValidEmail);
+
+    expect(sutReturn).toBeTruthy();
+  });
+});
