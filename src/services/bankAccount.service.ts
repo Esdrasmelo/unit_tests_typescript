722682b3cea75a84017ae2b44153956d648744e8
Esdrasmelo <emelomotta@gmail.com>
2023-02-02 10:24:07 -0300

    feat: add bankAccount service

--- src/services/bankAccount.service.ts
+++ src/services/bankAccount.service.ts
@@ -1,4 +1,7 @@
-import { createdResponse } from "../protocols/http.protocols";
+import {
+  badRequestResponse,
+  createdResponse,
+} from "../protocols/http.protocols";
 import { IHttpResponse } from "../types";
 import {
   IBankAccounUpdate,
@@ -14,10 +17,39 @@ export class BankAccountService {
     this.bankAccountRepositoryPort = bankAccountRepositoryPort;
   }
 
+  async BankAccountExists(accountNumber: string): Promise<boolean> {
+    try {
+      const bankAccount =
+        await this.bankAccountRepositoryPort.FindByAccountNumber(accountNumber);
+
+      if (bankAccount) return true;
+
+      return false;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   async CreateBankAccount(
     inputData: IBankAccountAdd
   ): Promise<IHttpResponse<IBankAccount | string>> {
     try {
+      const bankAccountAlreadyExists = await this.BankAccountExists(
+        inputData.accountNumber
+      );
+
+      if (bankAccountAlreadyExists) {
+        return badRequestResponse(
+          "A bank account for this account number already exists"
+        );
+      }
+
+      if (inputData.amountToBeDeposited === 0) {
+        return badRequestResponse<string>(
+          "You must deposit to be able to create a bank account"
+        );
+      }
+
       const createdBankAccount = await this.bankAccountRepositoryPort.Create(
         inputData
       );
