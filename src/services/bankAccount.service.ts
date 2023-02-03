05c33cfa2e9867fc7168415fc10eda4424e495e9
Esdrasmelo <emelomotta@gmail.com>
2023-02-03 14:16:14 -0300

    feat: validate if owner exists

--- src/services/bankAccount.service.ts
+++ src/services/bankAccount.service.ts
@@ -1,20 +1,27 @@
 import {
   badRequestResponse,
   createdResponse,
+  notFoundResponse,
 } from "../protocols/http.protocols";
-import { IHttpResponse } from "../types";
+import { IHttpResponse, IUserRepositoryPort } from "../types";
 import {
   IBankAccounUpdate,
   IBankAccount,
   IBankAccountAdd,
 } from "../types/bankAccount";
 import { IBankAccountRepositoryPort } from "../types/repository/bankAccountRepositoryPort.type";
+import { UserService } from "./user.service";
 
 export class BankAccountService {
   private bankAccountRepositoryPort: IBankAccountRepositoryPort;
+  private userService: UserService;
 
-  constructor(bankAccountRepositoryPort: IBankAccountRepositoryPort) {
+  constructor(
+    bankAccountRepositoryPort: IBankAccountRepositoryPort,
+    userService: UserService
+  ) {
     this.bankAccountRepositoryPort = bankAccountRepositoryPort;
+    this.userService = userService;
   }
 
   async BankAccountExists(accountNumber: string): Promise<boolean> {
@@ -37,11 +44,18 @@ export class BankAccountService {
       const bankAccountAlreadyExists = await this.BankAccountExists(
         inputData.accountNumber
       );
+      const ownerExists = await this.userService.UserExistsById(
+        inputData.owner_id
+      );
 
       if (bankAccountAlreadyExists) {
         return badRequestResponse(
           "A bank account for this account number already exists"
         );
+      }
+
+      if (!ownerExists) {
+        return notFoundResponse("Owner does not exist");
       }
 
       if (inputData.amountToBeDeposited === 0) {
